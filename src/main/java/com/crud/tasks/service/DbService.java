e30ca7a5034f69693bbbd989d1b169e94df760c3
pastorek89 <pastor.patryk@gmail.com>
2019-10-14 21:37:01 +0200

    19.2 update

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -17,7 +17,7 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskById(Long id) {
-        return repository.findById();
+    public Task getTaskById(final Long id) {
+        return repository.findById(id).orElse(null);
     }
 }
